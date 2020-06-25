5d8954f9fb59caa4dc62594904b46f7f1a76437f
Spencer Beaumont <spence.beaumont@gmail.com>
2020-06-25 00:30:01 -0600

    added new functionality to the NewHabitActivity

--- app/src/main/java/org/stbeaumont/habitjournal/controller/NewHabitActivity.java
+++ app/src/main/java/org/stbeaumont/habitjournal/controller/NewHabitActivity.java
@@ -11,8 +11,10 @@ import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Switch;
 import android.widget.TimePicker;
 
@@ -21,24 +23,42 @@ import com.google.android.material.textfield.TextInputEditText;
 import org.stbeaumont.habitjournal.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class NewHabitActivity extends AppCompatActivity {
 
-    TextInputEditText editTextHabit, editTextTime;
-    Button buttonDaily, buttonWeekly, buttonMonthly;
-    Button buttonSun, buttonMon, buttonTue, buttonWed, buttonThu, buttonFri, buttonSat;
-    Button buttonEveryday;
-    ConstraintLayout constraintDaily, constraintWeekly, constraintMonthly;
-    Switch switchGoal;
-    ConstraintLayout constraintGoal;
+    private static final int SUN = 0;
+    private static final int MON = 1;
+    private static final int TUE = 2;
+    private static final int WED = 3;
+    private static final int THU = 4;
+    private static final int FRI = 5;
+    private static final int SAT = 6;
 
+    private TextInputEditText editTextHabit, editTextTime;
+    private Button buttonDaily, buttonWeekly, buttonMonthly;
+    private Button buttonEveryday;
+    private Button buttonFirstDay, buttonLastDay;
+    private ConstraintLayout constraintDaily, constraintWeekly, constraintMonthly;
+    private ConstraintLayout constraintGoal;
+
+    private ArrayList<Button> dayButtons = new ArrayList<>();
+    private ArrayList<Button> frequencyButtons = new ArrayList<>();
+    private ArrayList<ConstraintLayout> frequencyConstraints = new ArrayList<>();
+    private ArrayList<Boolean> daysOfWeek = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_habit);
+
+        for (int i = 0; i < 7; i++) { //make it true for every day by default
+            daysOfWeek.add(true);
+        }
 
         editTextHabit = findViewById(R.id.editTextHabit);
         editTextTime = findViewById(R.id.editTextTime);
@@ -47,28 +67,120 @@ public class NewHabitActivity extends AppCompatActivity {
         buttonWeekly = findViewById(R.id.buttonWeekly);
         buttonMonthly = findViewById(R.id.buttonMonthly);
 
+        frequencyButtons.add(buttonDaily);
+        frequencyButtons.add(buttonWeekly);
+        frequencyButtons.add(buttonMonthly);
+
+        buttonFirstDay = findViewById(R.id.buttonFirst);
+        buttonLastDay = findViewById(R.id.buttonLast);
+
+        Button buttonSun = findViewById(R.id.buttonSunday);
+        Button buttonMon = findViewById(R.id.buttonMonday);
+        Button buttonTue = findViewById(R.id.buttonTuesday);
+        Button buttonWed = findViewById(R.id.buttonWednesday);
+        Button buttonThu = findViewById(R.id.buttonThursday);
+        Button buttonFri = findViewById(R.id.buttonFriday);
+        Button buttonSat = findViewById(R.id.buttonSaturday);
+
+        dayButtons.add(buttonSun);
+        dayButtons.add(buttonMon);
+        dayButtons.add(buttonTue);
+        dayButtons.add(buttonWed);
+        dayButtons.add(buttonThu);
+        dayButtons.add(buttonFri);
+        dayButtons.add(buttonSat);
+
+        buttonEveryday = findViewById(R.id.buttonEveryday);
+
         constraintDaily = findViewById(R.id.constraintDaily);
         constraintWeekly = findViewById(R.id.constraintWeekly);
         constraintMonthly = findViewById(R.id.constraintMonthly);
+        constraintGoal = findViewById(R.id.constraintGoal);
 
-        buttonDaily.setOnClickListener(new View.OnClickListener() {
+        frequencyConstraints.add(constraintDaily);
+        frequencyConstraints.add(constraintWeekly);
+        frequencyConstraints.add(constraintMonthly);
+
+        NumberPicker pickerWeeks = findViewById(R.id.numberPickerWeeks);
+        NumberPicker pickerDayOfMonth = findViewById(R.id.numberPickerDayOfMonth);
+        NumberPicker pickerGoal = findViewById(R.id.numberPickerGoal);
+
+        Switch switchGoal = findViewById(R.id.switchGoal);
+
+        switchGoal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                selectDaily();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    constraintGoal.setVisibility(View.VISIBLE);
+                } else {
+                    constraintGoal.setVisibility(View.GONE);
+                }
             }
         });
 
-        buttonWeekly.setOnClickListener(new View.OnClickListener() {
+        pickerWeeks.setMinValue(1);
+        pickerWeeks.setMaxValue(6);
+
+        pickerDayOfMonth.setMinValue(1);
+        pickerDayOfMonth.setMaxValue(31);
+
+        pickerGoal.setMinValue(1);
+        pickerGoal.setMaxValue(999);
+
+        for (int i = 0; i < 3; i++) {
+            final int finalI = i;
+            frequencyButtons.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectFrequency(finalI);
+                }
+            });
+        }
+
+        for (int i = 0; i < 7; i++) {
+            final int finalI = i;
+            dayButtons.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isEveryDay()) {
+                        for (int j = 0; j < 7; j++) {   //inverts the values
+                            boolean tempInverse = !daysOfWeek.get(j);
+                            daysOfWeek.set(j, tempInverse);
+                        }
+                        boolean temp = daysOfWeek.get(finalI);
+                        daysOfWeek.set(finalI, !temp);
+                    } else if (!isEveryDay() && isOnlyOne() && daysOfWeek.get(finalI)) {
+                        //do nothing
+                    } else {
+                        boolean temp = daysOfWeek.get(finalI);
+                        daysOfWeek.set(finalI, !temp);
+                    }
+                    updateDayButtons();
+                }
+            });
+        }
+
+        buttonEveryday.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectWeekly();
+                for (int i = 0; i < 7; i++) { //set every day to true
+                    daysOfWeek.set(i, true);
+                }
+                updateDayButtons();
             }
         });
 
-        buttonMonthly.setOnClickListener(new View.OnClickListener() {
+        buttonFirstDay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selectMonthly();
+                selectFirstDay();
+            }
+        });
+
+        buttonLastDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectLastDay();
             }
         });
 
@@ -91,44 +203,75 @@ public class NewHabitActivity extends AppCompatActivity {
             }
         });
 
+        updateDayButtons();
+
     }
 
-    public void selectDaily() {
-        buttonDaily.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonDaily.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonWeekly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonWeekly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonMonthly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonMonthly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-
-        constraintDaily.setVisibility(View.VISIBLE);
-        constraintWeekly.setVisibility(View.GONE);
-        constraintMonthly.setVisibility(View.GONE);
+    public void selectFrequency(int index) {
+        for (int i = 0; i < 3; i++) {
+            if (i == index) {
+                frequencyButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                frequencyButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                frequencyConstraints.get(i).setVisibility(View.VISIBLE);
+            } else {
+                frequencyButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                frequencyButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                frequencyConstraints.get(i).setVisibility(View.GONE);
+            }
+        }
     }
 
-    public void selectWeekly() {
-        buttonWeekly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonWeekly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonDaily.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonDaily.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonMonthly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonMonthly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-
-        constraintWeekly.setVisibility(View.VISIBLE);
-        constraintDaily.setVisibility(View.GONE);
-        constraintMonthly.setVisibility(View.GONE);
+    public void selectFirstDay() {
+        buttonFirstDay.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+        buttonFirstDay.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+        buttonLastDay.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+        buttonLastDay.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
     }
 
-    public void selectMonthly() {
-        buttonMonthly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonMonthly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonDaily.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonDaily.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
-        buttonWeekly.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
-        buttonWeekly.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+    public void selectLastDay() {
+        buttonLastDay.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+        buttonLastDay.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+        buttonFirstDay.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+        buttonFirstDay.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+    }
 
-        constraintMonthly.setVisibility(View.VISIBLE);
-        constraintDaily.setVisibility(View.GONE);
-        constraintWeekly.setVisibility(View.GONE);
+    public boolean isEveryDay() {
+        return (daysOfWeek.get(SUN) && daysOfWeek.get(MON) && daysOfWeek.get(TUE) && daysOfWeek.get(WED) && daysOfWeek.get(THU) && daysOfWeek.get(FRI) && daysOfWeek.get(SAT));
+    }
+
+    public boolean isOnlyOne() {
+        if (Collections.frequency(daysOfWeek, true) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void updateDayButtons() {
+        if (isEveryDay()) {
+            for (int i = 0; i < 7; i++) {
+                if (daysOfWeek.get(i)) {
+                    dayButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                    dayButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                } else {
+                    dayButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                    dayButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                }
+            }
+            buttonEveryday.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+            buttonEveryday.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+        } else {
+            for (int i = 0; i < 7; i++) {
+                if (daysOfWeek.get(i)) {
+                    dayButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                    dayButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                } else {
+                    dayButtons.get(i).setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+                    dayButtons.get(i).setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+                }
+            }
+            buttonEveryday.setTextColor(ContextCompat.getColor(NewHabitActivity.this, R.color.colorPrimary));
+            buttonEveryday.setBackgroundColor(ContextCompat.getColor(NewHabitActivity.this, R.color.white));
+        }
     }
 }
